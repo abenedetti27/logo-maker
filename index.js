75fca6651070a7c486fd36d00d9bdd768d7f4be7
abenedetti27 <145300062+abenedetti27@users.noreply.github.com>
2023-10-24 22:13:21 -0700

    svg library elements for shapes and color added

--- index.js
+++ index.js
@@ -23,6 +23,19 @@ function getUserInput() {
 
 function generateLogo(text, textColor, shape, shapeColor) {
     const draw = svg('logo').size(300, 200);
+    const textElement = draw.text(text).fill(textColor).move(50, 100);
+    let shapeElement;
+    if(shape === 'circle'){
+        shapeElement = draw.circle(100).fill(shapeColor).move(100, 50);   
+    } else if (shape === 'triangle') {
+        shapeElement = draw
+            .line(100, 150, 150, 50)
+            .stroke({width: 2, color: shapeColor})
+            .plot(150, 50, 200, 150)
+            .plot(100, 150, 200, 150)
+    } else if (shape === 'square') {
+        shapeElement = draw.rect(100, 100).fill(shapeColor).move(100, 50);
+    }
     const generatedSVG = draw.svg();
     fs.writeFileSync('logo.svg', generatedSVG);
     console.log('Generated SVG logo successfully!');
