4705e0dce2dfbc848b3e8803344078231a0c60c8
abenedetti27 <145300062+abenedetti27@users.noreply.github.com>
2023-10-24 21:29:21 -0700

    create const for dependencies within javascript as well as work through functions for generating logo and working through user inputs using readline.

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 //runs the application using import from lib
+
+const readline = require('readline');
+const fs = require('fs');
+const svg = require('svg.js');
+
+const { stdin: input, stdout: output } = require('node:process');
+
+const rl = readline.createInterface({ input, output });
+
+rl.question('Enter text: ', (text) => {
+    rl.question('Enter text color: ', (textColor) => {
+        rl.question('Choose a shape: ', (shape) => {
+            rl.question('Enter shape color: ', (shapeColor) => {
+                rl.close();
+                generateLogo(text, textColor, shape, shapeColor);
+            });
+        });
+    });
+});
+
+fs.writeFileSync('logo.svg', generatedSVG);
+
+
